a4546a0f38be4571bb1a12c6a48090d817340c8b
nicksideal <nicolas.fajardo@revature.net>
2020-01-09 16:46:52 -0500

    DAO impl

--- src/main/java/daos/QuizDaoImpl.java
+++ src/main/java/daos/QuizDaoImpl.java
@@ -10,41 +10,74 @@ import utility.ConnectionsUtil;
 import utility.MagicWords;
 
 public class QuizDaoImpl implements QuizDao{
+	
+	private static QuizDao instance = new QuizDaoImpl();
+	
+	private Quiz extractQuiz(ResultSet rs) {
+		return new Quiz(rs.getInt("quiz_id"), rs.getString("name"), 0, 0, null, null, null, null, null, 0, null);
+	}
 
 	@Override
 	public void CreateQuiz(int quiz_id, String name, int created_by) {
 		try (Connection c = ConnectionsUtil.getConnection()){
-			PreparedStatement stmt = c.prepareCall(MagicWords.LOGIN_QUERY);
-			stmt.setString(1, username);
-			stmt.setString(2, password);
+			PreparedStatement stmt = c.prepareCall(MagicWords.ADD_QUIZ);
+			stmt.setString(1, name);
+			stmt.setInt(2, created_by);
+			stmt.executeQuery();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	@Override
+	public void AddQuestion(int quiz_question_id, String question, int quiz_id) {
+		try (Connection c = ConnectionsUtil.getConnection()){
+			PreparedStatement stmt = c.prepareCall(MagicWords.ADD_QUESTION);
+			stmt.setString(1, question);
+			stmt.setInt(2, quiz_id);
+			stmt.executeQuery();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	@Override
+	public void AddAnswer(int question_answer_id, String answer_a, String answer_b, String answer_c, String answer_d,
+			String answer, int quiz_question_id) {
+		try (Connection c = ConnectionsUtil.getConnection()){
+			PreparedStatement stmt = c.prepareCall(MagicWords.ADD_ANSWER);
+			stmt.setString(1, answer_a);
+			stmt.setString(2, answer_b);
+			stmt.setString(3, answer_c);
+			stmt.setString(4, answer_d);
+			stmt.setInt(5, quiz_question_id);
+			stmt.executeQuery();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return;
+		
+	}
+
+	@Override
+	public Quiz SendQuiz(int quiz_id) {
+		try (Connection c = ConnectionsUtil.getConnection()){
+			PreparedStatement stmt = c.prepareCall(MagicWords.GET_QUIZZES);
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
-				return extractUser(rs);
+				return extractQuiz(rs);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	@Override
-	public void AddQuestion(int quiz_question_id, String question, int quiz_id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void AddAnswer(int question_answer_id, String answer_a, String answer_b, String answer_c, String answer_d,
-			String answer, int quiz_question_id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Quiz SendQuiz(int quiz_id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- src/main/java/utility/MagicWords.java
+++ src/main/java/utility/MagicWords.java
@@ -12,5 +12,5 @@ public class MagicWords {
 		public static final String ADD_QUIZ = "INSERT INTO quiz (quiz_id, name, created_by)" + "VALUES (quiz_id_seq.nextval, ?, ?)";
 		public static final String ADD_QUESTION = "INSERT INTO quiz_questions (quiz_question_id, question, quiz_id)" + "VALUES (quiz_question_id_seq.nextval, ?, ?)";
 		public static final String ADD_ANSWER = "INSERT INTO quiz_answers_multiplechoice (question_answer_id, answer_a, answer_b, answer_c, answer_d, quiz_question_id)" + "VALUES (question_answer_id_seq.nextval, ?, ?, ?, ?, ?)";
-
+		public static final String GET_QUIZZES = "SELECT * FROM quiz";
 }
